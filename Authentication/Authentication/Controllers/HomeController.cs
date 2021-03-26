e2e8f46c724972ebc70ddd4b076c6c0832f2c039
mdsobujislam <sobujislam674@gmail.com>
2021-03-26 19:06:21 +0600

    First Comment

---
+++ Authentication/Authentication/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Authentication.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Authentication.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ShowName()
+        {
+            string[] name = { "sobuj", "Ikbal", "Rahim", "Urmi" };
+            ViewBag.nm = name;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Authentication/Authentication/Data/MyUserClaimsPrincipalFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Authentication.Data
+{
+    public class MyUserClaimsPrincipalFactory:UserClaimsPrincipalFactory<ApplicationUser>
+    {
+        public MyUserClaimsPrincipalFactory(UserManager<ApplicationUser> userManager, IOptions<IdentityOptions> optionAccessor): base(userManager, optionAccessor)
+        {
+
+        }
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
+        {
+            var identity = await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("Name", user.Name?? "[Clickto Edit Profile]"));
+            identity.AddClaim(new Claim("CellPhone", user.CellPhone ?? "[Clickto Edit Profile]"));
+            identity.AddClaim(new Claim("Country", user.Country ?? "[Clickto Edit Profile]"));
+
+            return identity;
+        }
+    }
+}
